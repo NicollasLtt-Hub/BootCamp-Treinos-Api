f4924166d56303a1b13646961317bd93ac7b2721
Nicollas Lott <nicollaslottcurvellano@outlook.com>
2026-03-01 19:45:37 -0300

    fix: docker-compose connection

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,13 +3,14 @@
 import "dotenv/config";
 
 import { defineConfig } from "prisma/config";
-console.log(process.env["DATABASE_URL"]);
+import { env } from "process";
+const url = `postgresql://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_DATABASE}`;
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url,
   },
 });

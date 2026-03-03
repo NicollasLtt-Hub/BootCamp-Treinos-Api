758fafbcfc7b863d8376d95a71ab28fb747766b4
Nicollas Lott <nicollaslottcurvellano@outlook.com>
2026-03-02 22:53:47 -0300

    fix(auth): corrigir a falha de criação de cookies no Better Auth, ainda não resolvida

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ import {
 import { IncomingHttpHeaders } from "http";
 import z from "zod";
 
+import { NotFoundError } from "./errors/index.js";
 import { WeekDay } from "./generated/prisma/enums.js";
 import { auth } from "./lib/auth.js";
 import { CreateWorkoutPlan } from "./usecases/CreateWorkoutPlan.js";
@@ -79,7 +80,7 @@ app.withTypeProvider<ZodTypeProvider>().route({
       workoutDays: z.array(
         z.object({
           name: z.string().trim().min(1),
-          WeekDay: z.enum(WeekDay),
+          weekDay: z.nativeEnum(WeekDay),
           isRest: z.boolean().default(false),
           estimatedDurationInSeconds: z.number().min(1),
           exercises: z.array(
@@ -98,16 +99,25 @@ app.withTypeProvider<ZodTypeProvider>().route({
       201: z.object({
         id: z.uuid(),
         name: z.string().trim().min(1),
-        WeekDay: z.enum(WeekDay),
-        isRest: z.boolean().default(false),
-        estimatedDurationInSeconds: z.number().min(1),
-        exercises: z.array(
+        userId: z.string().trim().min(1),
+        isActive: z.boolean(),
+        workoutDays: z.array(
           z.object({
-            order: z.number().min(0),
+            id: z.uuid(),
             name: z.string().trim().min(1),
-            sets: z.number().min(1),
-            reps: z.number().min(1),
-            restTimeInSeconds: z.number().min(1),
+            weekDay: z.nativeEnum(WeekDay),
+            isRest: z.boolean(),
+            estimatedDurationInSeconds: z.number().min(1),
+            exercises: z.array(
+              z.object({
+                id: z.uuid(),
+                order: z.number().min(0),
+                name: z.string().trim().min(1),
+                sets: z.number().min(1),
+                reps: z.number().min(1),
+                restTimeInSeconds: z.number().min(1),
+              }),
+            ),
           }),
         ),
       }),
@@ -119,25 +129,47 @@ app.withTypeProvider<ZodTypeProvider>().route({
         error: z.string(),
         code: z.string(),
       }),
+      404: z.object({
+        error: z.string(),
+        code: z.string(),
+      }),
+      500: z.object({
+        error: z.string(),
+        code: z.string(),
+      }),
     },
   },
   handler: async (request, reply) => {
-    const session = await auth.api.getSession({
-      headers: fromNodeHeaders(request.headers),
-    });
-    if (!session) {
-      return reply.status(401).send({
-        error: "Unautorized",
-        code: "UNAUTHORIZED",
+    try {
+      const session = await auth.api.getSession({
+        headers: fromNodeHeaders(request.headers),
+      });
+      if (!session) {
+        return reply.status(401).send({
+          error: "Unautorized",
+          code: "UNAUTHORIZED",
+        });
+      }
+      const createWorkoutPlan = new CreateWorkoutPlan();
+      const result = await createWorkoutPlan.execute({
+        userId: session.user.id,
+        name: request.body.name,
+        workoutDays: request.body.workoutDays,
+      });
+      return reply.status(201).send(result);
+    } catch (error) {
+      app.log.error(error);
+      if (error instanceof NotFoundError) {
+        return reply.status(404).send({
+          error: error.message,
+          code: "NOT_FOUND_ERROR",
+        });
+      }
+      return reply.status(500).send({
+        error: "Internal server error",
+        code: "INTERNAL_SERVER_ERROR",
       });
     }
-    const createWorkoutPlan = new CreateWorkoutPlan();
-    const result = await createWorkoutPlan.execute({
-      userId: request.user.id,
-      name: request.body.name,
-      workoutDays: request.body.workoutDays,
-    });
-    return reply.status(201).send(result);
   },
 });
 
@@ -213,6 +245,22 @@ try {
   app.log.error(err);
   process.exit(1);
 }
+
 function fromNodeHeaders(headers: IncomingHttpHeaders): Headers {
-  throw new Error("Function not implemented.");
+  const result = new Headers();
+
+  for (const [key, value] of Object.entries(headers)) {
+    if (typeof value === "undefined") continue;
+
+    if (Array.isArray(value)) {
+      for (const v of value) {
+        result.append(key, v);
+      }
+      continue;
+    }
+
+    result.append(key, value);
+  }
+
+  return result;
 }
--- src/usecases/CreateWorkoutPlan.ts
+++ src/usecases/CreateWorkoutPlan.ts
@@ -1,3 +1,4 @@
+import { NotFoundError } from "../errors/index.js";
 import { WeekDay } from "../generated/prisma/enums.js";
 import { prisma } from "../lib/db.js";
 
@@ -27,61 +28,64 @@ interface InputDto {
 
 export class CreateWorkoutPlan {
   async execute(dto: InputDto) {
-    const existingWorkoutPlan = await prisma.workoutPlan.findFirst({
-      where: {
-        isActive: true,
-      },
-    });
     // Transaction - Atomicidade
     return prisma.$transaction(async (tx) => {
+      const existingWorkoutPlan = await tx.workoutPlan.findFirst({
+        where: {
+          isActive: true,
+          userId: dto.userId,
+        },
+      });
+
       if (existingWorkoutPlan) {
         await tx.workoutPlan.update({
           where: { id: existingWorkoutPlan.id },
           data: { isActive: false },
         });
-
-        const workoutPlan = await tx.workoutPlan.create({
-          data: {
-            id: crypto.randomUUID(),
-            name: dto.name,
-            userId: dto.userId,
-            isActive: true,
-            workoutDays: {
-              create: dto.workoutDays.map((workoutDay) => ({
-                name: workoutDay.name,
-                weekDay: workoutDay.weekDay,
-                isRest: workoutDay.isRest,
-                estimatedDurationInSeconds:
-                  workoutDay.estimatedDurationInSeconds,
-                exercises: {
-                  create: workoutDay.exercises.map((exercise) => ({
-                    name: exercise.name,
-                    order: exercise.order,
-                    sets: exercise.sets,
-                    reps: exercise.reps,
-                    restTimeInSeconds: exercise.restTimeInSeconds,
-                  })),
-                },
-              })),
-            },
-          },
-        });
-        const result = await tx.workoutPlan.findUnique({
-          where: { id: workoutPlan.id },
-          include: {
-            workoutDays: {
-              include: {
-                exercises: true,
-              },
-            },
-          },
-        });
-
-        if (!result) {
-          throw new Error("Workou plan not found");
-        }
-        return result;
       }
+
+      const workoutPlan = await tx.workoutPlan.create({
+        data: {
+          id: crypto.randomUUID(),
+          name: dto.name,
+          userId: dto.userId,
+          isActive: true,
+          workoutDays: {
+            create: dto.workoutDays.map((workoutDay) => ({
+              name: workoutDay.name,
+              weekDay: workoutDay.weekDay,
+              isRest: workoutDay.isRest,
+              estimatedDurationInSeconds: workoutDay.estimatedDurationInSeconds,
+              exercises: {
+                create: workoutDay.exercises.map((exercise) => ({
+                  name: exercise.name,
+                  order: exercise.order,
+                  sets: exercise.sets,
+                  reps: exercise.reps,
+                  restTimeInSeconds: exercise.restTimeInSeconds,
+                })),
+              },
+            })),
+          },
+        },
+      });
+
+      const result = await tx.workoutPlan.findUnique({
+        where: { id: workoutPlan.id },
+        include: {
+          workoutDays: {
+            include: {
+              exercises: true,
+            },
+          },
+        },
+      });
+
+      if (!result) {
+        throw new NotFoundError("Workout plan not found");
+      }
+
+      return result;
     });
   }
 }
